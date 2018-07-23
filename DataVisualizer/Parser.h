3b36fc76c6f7fa0dfed934db252d6742a8a1223e
slothriot <diffrectingbull@gmail.com>
2018-07-23 12:26:13 -0700

    separated Parser and Shader

--- DataVisualizer/Parser.h
+++ DataVisualizer/Parser.h
@@ -1,1 +1,40 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <string.h>
+#include <streambuf>
+#include <cerrno>
+#include <iomanip>      // std::setprecision
+
+std::string get_file_contents(const char *filename)
+{
+	std::ifstream in(filename, std::ios::in | std::ios::binary);
+	if (in)
+	{
+		return(std::string((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>()));
+	}
+	throw(errno);
+}
+void removeCharsFromString(std::string &str, char* charsToRemove) {
+	for (unsigned int i = 0; i < strlen(charsToRemove); ++i) {
+		str.erase(remove(str.begin(), str.end(), charsToRemove[i]), str.end());
+	}
+}
+
+double grabDouble(std::string &from)
+{
+	size_t indexOfNextWS = from.find(' ');
+	std::string token = from.substr(0, indexOfNextWS);
+
+	std::string::size_type sz;
+	long double grabbedDoub = std::stod(token, &sz);
+	if (from.length() > indexOfNextWS + 1)
+		from.erase(0, indexOfNextWS + 1);
+	return grabbedDoub;
+}
--- DataVisualizer/main.cpp
+++ DataVisualizer/main.cpp
@@ -11,7 +11,9 @@
 #include <algorithm>
 #include <sstream>
 #include <string.h>
-
+#include <streambuf>
+#include <cerrno>
+#include <iomanip>      // std::setprecision
 
 using namespace std;
 
@@ -29,101 +31,9 @@ using namespace glm;
 
 //include camera
 #include "Camera.h"
+#include "Parser.h"
+#include "Shader.h"
 
-GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_path) {
-
-	// Create the shaders
-	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
-
-	// Read the Vertex Shader code from the file
-	std::string VertexShaderCode;
-	std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
-	if (VertexShaderStream.is_open()) {
-		std::stringstream sstr;
-		sstr << VertexShaderStream.rdbuf();
-		VertexShaderCode = sstr.str();
-		VertexShaderStream.close();
-	}
-	else {
-		printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", vertex_file_path);
-		getchar();
-		return 0;
-	}
-
-	// Read the Fragment Shader code from the file
-	std::string FragmentShaderCode;
-	std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
-	if (FragmentShaderStream.is_open()) {
-		std::stringstream sstr;
-		sstr << FragmentShaderStream.rdbuf();
-		FragmentShaderCode = sstr.str();
-		FragmentShaderStream.close();
-	}
-
-	GLint Result = GL_FALSE;
-	int InfoLogLength;
-
-
-	// Compile Vertex Shader
-	printf("Compiling shader : %s\n", vertex_file_path);
-	char const * VertexSourcePointer = VertexShaderCode.c_str();
-	glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
-	glCompileShader(VertexShaderID);
-
-	// Check Vertex Shader
-	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
-	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if (InfoLogLength > 0) {
-		std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
-		glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
-		printf("%s\n", &VertexShaderErrorMessage[0]);
-	}
-
-
-
-	// Compile Fragment Shader
-	printf("Compiling shader : %s\n", fragment_file_path);
-	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
-	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
-	glCompileShader(FragmentShaderID);
-
-	// Check Fragment Shader
-	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
-	glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if (InfoLogLength > 0) {
-		std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
-		glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
-		printf("%s\n", &FragmentShaderErrorMessage[0]);
-	}
-
-
-
-	// Link the program
-	printf("Linking program\n");
-	GLuint ProgramID = glCreateProgram();
-	glAttachShader(ProgramID, VertexShaderID);
-	glAttachShader(ProgramID, FragmentShaderID);
-	glLinkProgram(ProgramID);
-
-	// Check the program
-	glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
-	glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if (InfoLogLength > 0) {
-		std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
-		glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
-		printf("%s\n", &ProgramErrorMessage[0]);
-	}
-
-
-	glDetachShader(ProgramID, VertexShaderID);
-	glDetachShader(ProgramID, FragmentShaderID);
-
-	glDeleteShader(VertexShaderID);
-	glDeleteShader(FragmentShaderID);
-
-	return ProgramID;
-}
 
 struct Point {
 	long double time;
@@ -148,37 +58,8 @@ Point* head;
 Point* currentPoint;
 
 
-#include <streambuf>
-#include <cerrno>
-#include <iomanip>      // std::setprecision
 
 
-std::string get_file_contents(const char *filename)
-{
-	std::ifstream in(filename, std::ios::in | std::ios::binary);
-	if (in)
-	{
-		return(std::string((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>()));
-	}
-	throw(errno);
-}
-void removeCharsFromString(string &str, char* charsToRemove) {
-	for (unsigned int i = 0; i < strlen(charsToRemove); ++i) {
-		str.erase(remove(str.begin(), str.end(), charsToRemove[i]), str.end());
-	}
-}
-
-double grabDouble(string &from)
-{
-	size_t indexOfNextWS = from.find(' ');
-	string token = from.substr(0, indexOfNextWS);
-
-	std::string::size_type sz;
-	long double grabbedDoub = std::stod(token, &sz);
-	if (from.length() > indexOfNextWS + 1)
-		from.erase(0, indexOfNextWS + 1);
-	return grabbedDoub;
-}
 // settings
 const unsigned int SCR_WIDTH = 1024;
 const unsigned int SCR_HEIGHT = 768;
